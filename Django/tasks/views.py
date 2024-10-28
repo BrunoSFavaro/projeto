6a01433453baf0fdf9e0b2b15a2008b0ab384caf
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-10-28 12:01:38 -0300

    "Add tasks" funcionando utilizando post method

--- Django/tasks/views.py
+++ Django/tasks/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
 
 tasks = ["foo", "bar", "baz"]
 
@@ -11,4 +14,35 @@ def index(request):
 
 # Add a new task:
 def add(request):
-    return render(request, "tasks/add.html")
+
+    # Check if method is post
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks 
+            tasks.append(task)
+
+            # Redirect user to list of tasks 
+            return HttpResponseRedirect(reverse("tasks:index"))
+        
+        else:
+
+            # If the form is invalid, re-render the page with existing information
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
