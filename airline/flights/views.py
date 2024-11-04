14b0ea904770de7a44812018bcb5d2643ee3c6b5
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-11-04 08:45:04 -0300

    Correção - erro ao adicionar passageiros ao voo

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Flight, Airport, Passenger
 
 # Create your views here.
