c65a887045894b090a2e8bea9925810410358717
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-12-12 08:48:31 -0300

    :sparkles: Criando novo diretório para aplicar Js

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('button').forEach(button => {
+    button.onclick = () => {
+      document.querySelector("#hello").style.color = button.dataset.color;
+    }
+  });
+});
