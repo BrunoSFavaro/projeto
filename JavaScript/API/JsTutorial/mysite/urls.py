de9d639cc463b5398ab6cd8e6d4dbdf2a94d6a8f
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-12-16 15:42:20 -0300

    :sparkles: Aplicação Django usando Js

---
+++ JavaScript/API/JsTutorial/mysite/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from mysite import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("sections/<int:num>", views.section, name="section"),
+]
