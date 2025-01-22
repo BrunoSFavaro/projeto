deb072a9f78abb20f89bcc72b835ead14a61ad69
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2025-01-22 12:02:56 -0300

    :sparkles: Implementações no arquivo test.py do Django

--- Testing/test.py
+++ Testing/test.py
@@ -2,22 +2,28 @@ import unittest
 from primetest import is_prime
 
 class Tests(unittest.TestCase):
-  def test1(self):
+  def test_1(self):
+    """Check that 1 is not prime"""
     self.assertFalse(is_prime(1))
 
-  def test2(self):
-    self.assertFalse(is_prime(2))
+  def test_2(self):
+    """Check that 2 is prime"""
+    self.assertTrue(is_prime(2))
 
-  def test8(self):
+  def test_8(self):
+    """Check that 8 is not prime"""
     self.assertFalse(is_prime(8))
 
-  def test11(self):
-    self.assertFalse(is_prime(11))
+  def test_11(self):
+    """Check that 11 is prime"""
+    self.assertTrue(is_prime(11))
 
-  def test25(self):
+  def test_25(self):
+    """Check that 25 is not prime"""
     self.assertFalse(is_prime(25))
 
-  def test28(self):
+  def test_28(self):
+    """Check that 28 is not prime"""
     self.assertFalse(is_prime(28))
 
 if __name__ == "__main__":
